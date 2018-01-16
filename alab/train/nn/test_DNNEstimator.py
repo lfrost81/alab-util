30b6465467dd21922e27c491fa2fea021cdcf50f
younghyun.kwon <dudgus81@hotmail.com>
2018-01-16 19:16:49 +0900

    fixed test code for classification

--- alab/train/nn/test_DNNEstimator.py
+++ alab/train/nn/test_DNNEstimator.py
@@ -2,54 +2,61 @@ from alab.train.nn import DNNEstimator
 from alab.data import NumpyDataFeeder
 from unittest import TestCase
 
-import tensorflow as tf
-import os
-import json
-
 
 class TestDNNEstimator(TestCase):
-    conf = {
-        'epoch': 500,
-        'learning_rate': 0.01,
-        'cost_func': 'cross_entropy',
-        'softmax': True,
-        'optimizer': 'adam',
-        'l2reg_lambda': 0,
-        'initializer': 'xavier',
-        'hidden_layers': [
-            {
-                'dim': 100,
-                'activate_func': 'relu',
-                'dropout': 0.9
-            }, {
-                'dim': 50,
-                'activate_func': 'relu',
-                'dropout': 0.9
-            }
-        ]
-    }
+    def setUp(self):
+        self.conf = {
+            'epoch': 1000,
+            'learning_rate': 0.05,
+            'cost_func': 'cross_entropy',
+            'softmax': True,
+            'optimizer': 'adam',
+            'l2reg_lambda': 0,
+            'initializer': 'xavier',
+            'hidden_layers': [
+                {
+                    'dim': 100,
+                    'activate_func': 'relu',
+                    'dropout': 0.99
+                }, {
+                    'dim': 33,
+                    'activate_func': 'sigmoid',
+                    'dropout': 0.99
+                },
+            ]
+        }
+
+        # Input of XOR Problem(0^0=0, 0^1=1, 1^0=1, 1^1=0)
+        self.x = [[0, 0], [0, 1], [1, 0], [1, 1]]
+        self.y = [[1, 0], [0, 1], [0, 1], [1, 0]]
 
     def test_fit(self):
-        x = [[0, 0], [0, 1], [1, 0], [1,1]]
-        y = [[0], [1], [1], [0]]
-
-        train = DNNEstimator(self.conf, verbosity=0)
-        train.fit(x, y)
-        accuracy = train.evaluate(x, y)
-        self.assertGreater(accuracy, 0.5)
+        """
+        Test whether model solve XOR classification
+        :return: None
+        """
+        train = DNNEstimator(self.conf, verbosity=2)
+        train.fit(self.x, self.y)
+        accuracy = train.evaluate(self.x, self.y)
+        self.assertEqual(int(accuracy), 1)
+        print('Accuracy:', accuracy)
+        print('Prediction Result:', train.predict(self.x))
 
     def test_fit_batch(self):
-        x = [[0, 0], [0, 1], [1, 0], [1,1]]
-        y = [[0], [1], [1], [0]]
-
+        """
+        Test whether model solve XOR classification on mini batch mode
+        :return: None
+        """
         feeder = NumpyDataFeeder(fetch_size=4)
-        feeder.add_data('x', x)
-        feeder.add_data('y', y)
+        feeder.add_data('x', self.x)
+        feeder.add_data('y', self.y)
 
-        train = DNNEstimator(self.conf, verbosity=0)
+        train = DNNEstimator(self.conf, verbosity=2)
 
         train.fit_batch(feeder)
-        accuracy = train.evaluate(x, y)
-        self.assertGreater(accuracy, 0.5)
+        accuracy = train.evaluate(self.x, self.y)
+        self.assertEqual(int(accuracy), 1)
+        print('Accuracy:', accuracy)
+        print('Prediction Result:', train.predict(self.x))
 
 
